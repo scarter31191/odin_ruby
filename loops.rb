dd896103e7cd65292df1413a2ae5edc56e4aa915
scarter31191 <scarter31191@gmaile.com>
2021-09-26 23:26:00 -0400

    removed comments

---
+++ loops.rb
@@ -0,0 +1,45 @@
+
+
+# loop {
+#     puts "i is #{i}"     
+#     break if i == 5               
+# }
+    
+# i = 0
+#  while i < 10 do 
+#     puts "#{i} is less than 10"
+#     i += 1
+# end
+
+# while gets.chomp != "yes" do
+#     puts "c'mon!"
+# end
+
+
+# until i >= 10 do 
+#    puts "i is #{i}"
+#    i += 1 
+# end
+
+
+# until gets.chomp == "yes" do 
+#     puts "but i love you"
+# end
+
+
+# i = 0
+# for i in 0..5 
+#     puts "#{i} zombies coming!"
+# end
+
+
+
+5.times { 
+    puts "Hello World!"
+}
+
+
+
+10.times do |num| 
+    puts "#{num}"
+end
